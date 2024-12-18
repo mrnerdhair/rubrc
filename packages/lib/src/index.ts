f91c62cb28924607ec76661be11775e733d7d094
MrNerdHair <mrnerdhair@gmail.com>
2024-12-18 16:53:23 -0600

    wip

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,3 +1,5 @@
-import "./sysroot";
-import "./get_rustc_wasm";
-import "./get_llvm_wasm";
+import * as sysroot from "./sysroot";
+import { get_rustc_wasm } from "./get_rustc_wasm";
+import { get_llvm_wasm } from "./get_llvm_wasm";
+
+export { sysroot, get_rustc_wasm, get_llvm_wasm };
