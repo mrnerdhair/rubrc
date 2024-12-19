ceaecbbf33e1718d2644c90e45abbeec57e1f380
MrNerdHair <mrnerdhair@gmail.com>
2024-12-19 16:38:16 -0600

    fix static asset decompression

--- packages/lib/src/get_rustc_wasm.ts
+++ packages/lib/src/get_rustc_wasm.ts
@@ -1,4 +1,4 @@
-import rustc_opt_wasm_br_url from "../data/rustc_opt.wasm?url";
+import rustc_opt_wasm_url from "../data/rustc_opt.wasm.br.dummyext?url";
 import { get_wasm } from "./get_wasm";
 
-export const get_rustc_wasm = () => get_wasm(rustc_opt_wasm_br_url);
+export const get_rustc_wasm = () => get_wasm(rustc_opt_wasm_url);
