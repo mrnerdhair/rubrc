658374b92962dbb17b17a13c50be7fd0fc32dc40
MrNerdHair <mrnerdhair@gmail.com>
2024-12-19 15:09:07 -0600

    reduce use of type assertions; add util package

---
+++ packages/util/src/index.ts
@@ -0,0 +1,12 @@
+export {
+  type WasiP1Cmd,
+  as_wasi_p1_cmd,
+  assert_wasi_p1_cmd,
+  is_wasi_p1_cmd,
+} from "./wasi_p1_cmd";
+export {
+  type WasiP1Thread,
+  as_wasi_p1_thread,
+  assert_wasi_p1_thread,
+  is_wasi_p1_thread,
+} from "./wasi_p1_thread";
---
+++ packages/util/src/wasi_p1_cmd.ts
@@ -0,0 +1,25 @@
+export type WasiP1Cmd = WebAssembly.Instance & {
+  exports: { memory: WebAssembly.Memory; _start: () => unknown };
+};
+
+export function is_wasi_p1_cmd(
+  value: WebAssembly.Instance,
+): value is WasiP1Cmd {
+  return (
+    "memory" in value.exports &&
+    value.exports.memory instanceof WebAssembly.Memory &&
+    "_start" in value.exports &&
+    typeof value.exports._start === "function"
+  );
+}
+
+export function assert_wasi_p1_cmd(
+  value: WebAssembly.Instance,
+): asserts value is WasiP1Cmd {
+  if (!is_wasi_p1_cmd(value)) throw new Error("expected WASI P1 command");
+}
+
+export function as_wasi_p1_cmd(value: WebAssembly.Instance): WasiP1Cmd {
+  assert_wasi_p1_cmd(value);
+  return value;
+}
