26c675eef37d04fad8c209743a7e760c035d3879
MrNerdHair <mrnerdhair@gmail.com>
2024-12-20 16:32:33 -0600

    clean up usage of import/export

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,5 +1,3 @@
-import { get_llvm_wasm } from "./get_llvm_wasm";
-import { get_rustc_wasm } from "./get_rustc_wasm";
-import * as sysroot from "./sysroot";
-
-export { sysroot, get_rustc_wasm, get_llvm_wasm };
+export { get_llvm_wasm } from "./get_llvm_wasm";
+export { get_rustc_wasm } from "./get_rustc_wasm";
+export * as sysroot from "./sysroot";
