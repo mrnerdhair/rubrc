feea134d34a46e95888922bcc787ea9afa16685e
MrNerdHair <mrnerdhair@gmail.com>
2024-12-20 16:33:17 -0600

    consistently use WASIFarmRefUseArrayBufferObject vs WASIFarmRefObject

--- packages/threads/src/index.ts
+++ packages/threads/src/index.ts
@@ -1,4 +1,7 @@
 export { WASIFarmAnimal } from "./animals";
 export { WASIFarm } from "./farm";
 export { WASIFarmRef, type WASIFarmRefObject } from "./ref";
-export { thread_spawn_on_worker } from "./shared_array_buffer/index";
+export {
+  thread_spawn_on_worker,
+  type WASIFarmRefUseArrayBufferObject,
+} from "./shared_array_buffer/index";
