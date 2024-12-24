9018eb799c2fbe881de3d5885181ec51e4e43688
MrNerdHair <mrnerdhair@gmail.com>
2024-12-23 21:04:52 -0600

    !fup add util

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,26 +1,15 @@
-import {
+export {
   type WasiP1Cmd,
   as_wasi_p1_cmd,
   assert_wasi_p1_cmd,
   is_wasi_p1_cmd,
 } from "./wasi_p1_cmd";
-import {
+export {
   type WasiP1Thread,
   as_wasi_p1_thread,
   assert_wasi_p1_thread,
   is_wasi_p1_thread,
 } from "./wasi_p1_thread";
-
-export {
-  type WasiP1Cmd,
-  is_wasi_p1_cmd,
-  assert_wasi_p1_cmd,
-  as_wasi_p1_cmd,
-  type WasiP1Thread,
-  is_wasi_p1_thread,
-  assert_wasi_p1_thread,
-  as_wasi_p1_thread,
-};
 
 export { setTransferHandlers } from "./comlink";
 
