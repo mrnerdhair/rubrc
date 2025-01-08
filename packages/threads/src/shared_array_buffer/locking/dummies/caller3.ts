8f0cd1d801512091019fae228d80a6431ee371cd
MrNerdHair <mrnerdhair@gmail.com>
2025-01-08 14:28:07 -0600

    dummy caller 3: make identical to dummy caller 1

--- packages/threads/src/shared_array_buffer/locking/dummies/caller3.ts
+++ packages/threads/src/shared_array_buffer/locking/dummies/caller3.ts
@@ -6,11 +6,19 @@ export class DummyCaller3 {
   }
 
   call_and_wait_blocking(): void {
-    const old = Atomics.exchange(this.notify_view, 0, 1);
-    Atomics.notify(this.notify_view, 0, 1);
+    const old = Atomics.compareExchange(this.notify_view, 0, 0, 1);
     if (old !== 0) {
       throw new Error("what happened?");
     }
+
+    const n = Atomics.notify(this.notify_view, 0, 1);
+    if (n !== 1) {
+      if (n !== 0) {
+        throw new Error(`invoke_fd_func notify failed: ${n}`);
+      }
+      console.warn("invoke_func_loop is late");
+    }
+
     const lock = Atomics.wait(this.notify_view, 0, 1);
     if (lock === "timed-out") {
       throw new Error("timed-out lock");
