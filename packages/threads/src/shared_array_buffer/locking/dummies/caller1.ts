5ec1c0655f6de0ccd0d66a8e018b2bf3527cb3ad
MrNerdHair <mrnerdhair@gmail.com>
2025-01-08 14:23:10 -0600

    dummy caller 1 & 2: compareExchange

--- packages/threads/src/shared_array_buffer/locking/dummies/caller1.ts
+++ packages/threads/src/shared_array_buffer/locking/dummies/caller1.ts
@@ -6,7 +6,7 @@ export class DummyCaller1 {
   }
 
   call_and_wait_blocking(): void {
-    const old = Atomics.exchange(this.notify_view, 0, 1);
+    const old = Atomics.compareExchange(this.notify_view, 0, 0, 1);
     if (old !== 0) {
       throw new Error("what happened?");
     }
