a44c1f8c6892c9e422ccb038a6d878a5346a3505
MrNerdHair <mrnerdhair@gmail.com>
2025-01-08 13:47:22 -0600

    dummy caller 3: normalize call() code parameter

--- packages/threads/src/shared_array_buffer/locking/dummies/caller3.ts
+++ packages/threads/src/shared_array_buffer/locking/dummies/caller3.ts
@@ -7,9 +7,8 @@ export class DummyCaller3 {
     this.notify_view = notify_view;
   }
 
-  call(_code: number): void {
-    // notify error = code 1
-    const old = Atomics.compareExchange(this.notify_view, 0, 0, 1);
+  call(code: number): void {
+    const old = Atomics.compareExchange(this.notify_view, 0, 0, code);
 
     if (old !== 0) {
       throw new Error("what happened?");
