bfe2a42c3211fb1c3a5d85fac2238cfdd5e443fb
MrNerdHair <mrnerdhair@gmail.com>
2025-01-08 14:21:04 -0600

    make dummy callers 1 & 2 more similar

--- packages/threads/src/shared_array_buffer/locking/dummies/caller1.ts
+++ packages/threads/src/shared_array_buffer/locking/dummies/caller1.ts
@@ -7,9 +7,25 @@ export class DummyCaller1 {
 
   call_and_wait_blocking(): void {
     const old = Atomics.exchange(this.notify_view, 0, 1);
-    Atomics.notify(this.notify_view, 0, 1);
     if (old !== 0) {
       throw new Error("what happened?");
+    }
+
+    const n = Atomics.notify(this.notify_view, 0, 1);
+    if (n !== 1) {
+      if (n !== 0) {
+        throw new Error(`invoke_fd_func notify failed: ${n}`);
+      }
+      // const len = this.get_fds_len();
+      // if (len <= this.fd) {
+      //   const lock = Atomics.exchange(this.notify_view, 0, 0);
+      //   if (lock !== 1) {
+      //     throw new Error("what happened?");
+      //   }
+      //   Atomics.notify(this.notify_view, 0, 1);
+      //   throw new Error(`what happened?: len ${len} fd ${this.fd}`);
+      // }
+      console.warn("invoke_func_loop is late");
     }
 
     const lock = Atomics.wait(this.notify_view, 0, 1);
--- packages/threads/src/shared_array_buffer/locking/dummies/caller2.ts
+++ packages/threads/src/shared_array_buffer/locking/dummies/caller2.ts
@@ -20,28 +20,29 @@ export class DummyCaller2 {
   call_and_wait_blocking(): void {
     const old = Atomics.exchange(this.notify_view, 0, 1);
     if (old === 1) {
-      throw new Error(`invoke_fd_func already invoked\nfd: ${this.fd}`);
+      throw new Error("what happened?");
     }
-    const n = Atomics.notify(this.notify_view, 0);
+
+    const n = Atomics.notify(this.notify_view, 0, 1);
     if (n !== 1) {
       if (n !== 0) {
         throw new Error(`invoke_fd_func notify failed: ${n}`);
       }
-      const len = this.get_fds_len();
-      if (len <= this.fd) {
-        const lock = Atomics.exchange(this.notify_view, 0, 0);
-        if (lock !== 1) {
-          throw new Error("what happened?");
-        }
-        Atomics.notify(this.notify_view, 0, 1);
-        throw new Error(`what happened?: len ${len} fd ${this.fd}`);
-      }
+      // const len = this.get_fds_len();
+      // if (len <= this.fd) {
+      //   const lock = Atomics.exchange(this.notify_view, 0, 0);
+      //   if (lock !== 1) {
+      //     throw new Error("what happened?");
+      //   }
+      //   Atomics.notify(this.notify_view, 0, 1);
+      //   throw new Error(`what happened?: len ${len} fd ${this.fd}`);
+      // }
       console.warn("invoke_func_loop is late");
     }
 
-    const value = Atomics.wait(this.notify_view, 0, 1);
-    if (value === "timed-out") {
-      throw new Error("wait call park_fd_func timed-out");
+    const lock = Atomics.wait(this.notify_view, 0, 1);
+    if (lock === "timed-out") {
+      throw new Error("timed-out lock");
     }
   }
 }
